7f99d79d58ce592748d4a2360de5c07220a775b6
Nyjel <nyjel.dukes@gmail.com>
2019-12-02 17:48:33 -0500

    Began work on the framework. Fragments almost set up.

--- app/src/main/java/org/computermentors/smeelslikebakin/ListAdapter.java
+++ app/src/main/java/org/computermentors/smeelslikebakin/ListAdapter.java
@@ -1,7 +1,53 @@
 package org.computermentors.smeelslikebakin;
 
-import androidx.appcompat.app.AlertController;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ListAdapter extends Recy {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
+public class ListAdapter extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ((ListViewHolder) holder).bindView(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return Recipes.names.length;
+    }
+
+    private class ListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        private TextView textView;
+        private ImageView imageView;
+
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.itemText);
+            imageView = itemView.findViewById(R.id.itemImage);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bindView(int position){
+            textView.setText(Recipes.names[position]);
+            imageView.setImageResource(Recipes.resourceIds[position]);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
 }
