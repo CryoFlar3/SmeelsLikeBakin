54bc384de5023fcaf401d8df6b19f9b76ffd9576
Nyjel <nyjel.dukes@gmail.com>
2019-12-04 16:13:19 -0500

    List populates with recipes

--- app/src/main/java/org/computermentors/smeelslikebakin/ListFragment.java
+++ app/src/main/java/org/computermentors/smeelslikebakin/ListFragment.java
@@ -7,12 +7,21 @@ import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.listRecyclerView);
+        ListAdapter listAdapter = new ListAdapter();
+        recyclerView.setAdapter(listAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
         return view;
     }
 }
